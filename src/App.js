aa9069a2c70a157687281e3fe5dbdbf498f39014
Nastya Samokhina <nasty.samohina@gmail.com>
2022-11-28 22:00:52 +0300

    dz1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Message from "./components/message/message";
 
 class App extends React.Component{
-    data = 'Как дела'
+    data = 'Как дела?'
 
     render() {
         return (<Message text={this.data}/>);
